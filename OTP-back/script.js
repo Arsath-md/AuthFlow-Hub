6766cd9558e8cd3aa56a0a0f58740d0ef60d5dfb
Arsath-md <mdarsath.m2003@gmail.com>
2026-05-25 13:00:43 +0530

    created at 25/5/26

--- OTP-back/script.js
+++ OTP-back/script.js
@@ -1,21 +1,27 @@
-const { config } = require("dotenv");
-const express = require("express")
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
-const route = require("./routes/routes")
-const cors = require("cors")
 
-require('dotenv'),config()
-app.use(express.json())
-app.use(cors())
+const route = require("./routes/routes");
+const cors = require("cors");
+const mongo = require("mongoose");
 
-
-const mongo = require("mongoose")
+app.use(express.json());
+app.use(cors());
 
 mongo.connect(process.env.MONGO_URL)
-    .then(()=> console.log("mongo db is connected"))
-    .catch((err)=> console.error("there is an error in mongo connect"+err))
-app.use("/",route)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) =>
+    console.error("MongoDB connection error:", err)
+  );
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running in the port:${process.env.PORT} `)
-})
+app.use("/", route);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
